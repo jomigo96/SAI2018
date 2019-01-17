0482fd0934249eb1fe32c1c7ccbae57c32e0ad19
jomigo96 <jomigo96@yahoo.com>
2019-01-17 16:07:12 +0000

    Kickstart ILS project

---
+++ Project/ils.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <GL/gl.h>
+#include <SDL2/SDL.h>
+
+int main(void){
+
+SDL_Window* window;
+window =  SDL_CreateWindow("ILS display", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 480, 360, 0); 
+ 
+getchar();
+	return 0;
+}
